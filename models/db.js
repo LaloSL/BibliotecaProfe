edad70c36829c9779e229a923f35adfc79657cf3
LaloSL <127783873+LaloSL@users.noreply.github.com>
2026-06-05 08:34:22 -0300

    configurar conexion a neon

--- models/db.js
+++ models/db.js
@@ -2,14 +2,17 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.DATABASE_URL,
   {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
     dialect: 'postgres',
+    protocol: 'postgres',
     logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 );
 
