fa0fb47e4d6f90bb486e3e8adaad9711903be483
LaloSL <127783873+LaloSL@users.noreply.github.com>
2026-06-05 15:44:05 -0300

    vuelta atras de la deoendencia pg

--- models/db.js
+++ models/db.js
@@ -1,10 +1,10 @@
 import 'dotenv/config';
 import { Sequelize } from 'sequelize';
-import pg from 'pg';
+//import pg from 'pg';
 
 const sequelize = new Sequelize({
   dialect: 'postgres',
-  dialectModule: pg,
+  //dialectModule: pg,
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   database: process.env.DB_NAME,
